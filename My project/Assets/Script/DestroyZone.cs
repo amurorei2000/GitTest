2c4df140081a078f295f9757417b04a5569aa40a
Wonseok Park <amurorei2000@gmail.com>
2022-07-05 10:54:57 +0900

    List 오브젝트풀

--- My project/Assets/Script/DestroyZone.cs
+++ My project/Assets/Script/DestroyZone.cs
@@ -10,7 +10,13 @@ public class DestroyZone : MonoBehaviour
         // 만약 부딪힌 녀석이 총알이라면
         if (collision.gameObject.name.Contains("Bullet"))
         {
-            // -> 탄창에 넣어주고
+            // -> 탄창에 넣어주고 싶다.
+            // 1. Player 가 있어야한다.
+            GameObject player = GameObject.Find("Player");
+            // 2. PlayerFire 가 있어야한다.
+            PlayerFire pf = player.GetComponent<PlayerFire>();
+            // 3. 탄창이 있어야한다.
+            pf.bulletPool.Add(collision.gameObject);
             collision.gameObject.SetActive(false);
         }
         // 그렇지 않으면
--- My project/Assets/Script/PlayerFire.cs
+++ My project/Assets/Script/PlayerFire.cs
@@ -6,26 +6,28 @@ using UnityEngine;
 // 필요속성 : 총알공장, 총구위치(주인공위치)
 // 탄창에서 총알을 발사하도록 하고 싶다.
 // 필요속성 : 탄창, 탄창에 넣을 총알 개수
+// List 를 이용하도록 탄창을 바꿔보자
 public class PlayerFire : MonoBehaviour
 {
     // 필요속성 : 총알공장
     public GameObject bulletFactory;
     // 필요속성 : 탄창, 탄창에 넣을 총알 개수
     public int bulletPoolSize = 10;
-    GameObject[] bulletPool;
+    //GameObject[] bulletPool;
+    public List<GameObject> bulletPool = new List<GameObject>();
 
     // Start is called before the first frame update
     void Start()
     {
         // 태어날 때 탄창에 총알을 만들어서 넣고 싶다.
         // 1. 탄창이 있어야한다.
-        bulletPool = new GameObject[bulletPoolSize];
+        //bulletPool = new GameObject[bulletPoolSize];
         for (int i = 0; i < bulletPoolSize; i++)
         {
             // 2. 총알이 있어야한다.
             GameObject bullet = Instantiate(bulletFactory);
             // 3. 탄창에 총알을 넣고싶다.
-            bulletPool[i] = bullet;
+            bulletPool.Add(bullet);
             bullet.SetActive(false);
         }
     }
@@ -40,19 +42,18 @@ public class PlayerFire : MonoBehaviour
         if (Input.GetButtonDown("Fire1"))
         {
             //총알 발사 시 탄창에서 총알을 가져와 발사하고 싶다.
-            for (int i = 0; i < bulletPoolSize; i++)
+            // -> 탄창에 총알이 있다면 맨위에 있는 한발 발사하고 싶다.
+            if(bulletPool.Count > 0)
             {
                 // 단, 비활성화 되어 있는 총알을 찾아서 발사하고 싶다.
                 // 2. 총알이 있어야한다.
-                GameObject bullet = bulletPool[i];
-                // 만약 총알이 비활성화 되어 있다면
-                if (bullet.activeSelf == false)
-                {
-                    bullet.SetActive(true);
-                    // 3. 총알을 총구앞에 놓기
-                    bullet.transform.position = transform.position;
-                    break;
-                }
+                GameObject bullet = bulletPool[0];
+                bullet.SetActive(true);
+                // 3. 총알을 총구앞에 놓기
+                bullet.transform.position = transform.position;
+
+                // 탄창에서 총알을 제거하기
+                bulletPool.RemoveAt(0);
             }
         }
     }
